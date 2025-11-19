c2a0c8240b07594bdd3d7f14b7136ab22fbe3d0b
akostiuk11 <akostiuk@mednet.ucla.edu>
2025-11-19 15:28:30 -0800

    finalized

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from stats import count_words
 from stats import char_count
 from stats import sort_dict
@@ -7,13 +8,19 @@ def get_book_text(file_path):
         return f.read()
 
 def main():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     #print(get_book_text("books/frankenstein.txt"))
-    count_words(get_book_text("books/frankenstein.txt"))
+    #count_words(get_book_text("books/frankenstein.txt"))
+    count_words(get_book_text(book_path))
     #print(char_count(get_book_text("books/frankenstein.txt")))
 
-    sorted_dict = sort_dict(char_count(get_book_text("books/frankenstein.txt")))
+    sorted_dict = sort_dict(char_count(get_book_text(book_path)))
     for d in sorted_dict:
         if d["char"].isalpha():
             print(f"{d['char']}: {d['num']}")
 
+    
 main()
